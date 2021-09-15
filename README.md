effceebd5de2f0d41a6bc651a62c1f115e744f0a
Marko Sipka <msipka@wollson.co>
2021-09-15 11:08:19 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# zip-bank
+## Hello Life
+
+
+> ### **_SCSS_**
+```
+Core
+Functions
+Mixins
+Variables
+Layouts
+Components
+Blocks
+Pages
+```
+
+
+## Setup
+```
+git clone
+npm install
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    order = require('gulp-order'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/assets/css/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('script', function() {
+  gulp.src('app/js/**/*.js')
+    .pipe(order([
+      "plugins/jquery.js",
+      "plugins/jquery.init.js",
+      "plugins/swiper.js",
+      "*.js",
+    ]))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('app/assets/js/'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('html', function() {
+   gulp.src('app/*.html')
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('watch', ['browserSync'], function() {
+   gulp.watch('app/scss/**/*.scss', ['sass']);
+   gulp.watch('app/js/**/*.js', ['script']);
+   gulp.watch('app/*.html', ['html']);
+});
+
+gulp.task('browserSync', function() {
+   browserSync.init ({
+       ghostMode: false,
+       server: {
+           baseDir: 'app',
+       },
+   })
+});
+
+gulp.task('default', ['sass', 'watch', 'html', 'script'])
